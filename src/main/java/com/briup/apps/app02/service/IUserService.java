a7a223d10195aa585b763ebbf469c99248c823a3
Cream-Q <2511551668@qq.com>
2019-06-05 23:02:21 +0800

    shixun

---
+++ src/main/java/com/briup/apps/app02/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.briup.apps.app02.service;
+
+import com.briup.apps.app02.bean.User;
+
+import java.util.List;
+
+public interface IUserService {
+    List<User> findAll();
+    List<User> query(User user);
+    User findById(long id);
+
+    void saveOrUpdate(User user) throws Exception ;
+
+    void deleteById(long id) throws Exception ;
+}
+
